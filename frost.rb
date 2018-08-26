e57d5304c7a915cbe4fe90687b08d54d795e74e4
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-27 08:51:02 +0930

    Brew formula update for frost version v0.0.1

---
+++ frost.rb
@@ -0,0 +1,11 @@
+class Frost < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/ThatTomPerson/frost/releases/download/v0.0.1/frost_0.0.1_darwin_amd64.tar.gz"
+  version "0.0.1"
+  sha256 "e747518463007cb2ed32a1c9bca19e9d3f22ac65ddf57a47b23ecd0c11cc71ea"
+
+  def install
+    bin.install "frost"
+  end
+end
