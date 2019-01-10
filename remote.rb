24dae1b8be96a1176fa856969de158e38cf03752
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-10 18:33:04 +1030

    Brew formula update for remote version v2.2.1

--- remote.rb
+++ remote.rb
@@ -1,9 +1,9 @@
 class Remote < Formula
   desc ""
   homepage ""
-  url "https://github.com/thattomperson/remote/releases/download/v2.2.0/remote_2.2.0_darwin_amd64.tar.gz"
-  version "2.2.0"
-  sha256 "853557ecc17bd16644f9a00afd5a508d0980cf601ab9fd19a523d3602d1abfe7"
+  url "https://github.com/thattomperson/remote/releases/download/v2.2.1/remote_2.2.1_darwin_amd64.tar.gz"
+  version "2.2.1"
+  sha256 "3c4ecf6e77b1ea3d937b8ac37df3c70e4cc6a4070c18ff08973941e75b214f5c"
 
   def install
     bin.install "remote"
