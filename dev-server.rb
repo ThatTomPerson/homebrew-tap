546beb779c767c09078849299b35c98ae067e2c5
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-24 13:07:37 +1030

    Brew formula update for dev-server version 0.0.12

--- dev-server.rb
+++ dev-server.rb
@@ -3,7 +3,7 @@ class DevServer < Formula
   homepage ""
   url "https://github.com/ThatTomPerson/dev-server/releases/download/0.0.12/dev-server_0.0.12_darwin_amd64.tar.gz"
   version "0.0.12"
-  sha256 "72e574fb166a92957841b836e37068e07a340e5288dc9d03c7d989c60bf55f61"
+  sha256 "88de2d89ae910c0503ce1bc44eb8e1b14dfe745bca31d7efab8c34363c34b22a"
   
   depends_on "mkcert"
 
